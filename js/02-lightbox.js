3cd51074b50023e51f3cf4bc3cd5edadd8c60545
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2022-12-09 20:09:03 +0200

    Make task - 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,31 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// ! Получил доступ к диву
+const galleryEl = document.querySelector('.gallery');
+
+// ! Создал разметку
+const markup = galleryItems.reduce
+((acc,{preview, original, description}) => acc +
+`<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt='${description}'/>
+</a>`
+,'');
+
+// ! Добавил разметку
+galleryEl.insertAdjacentHTML('beforeend', markup);
+
+// ! Повесил слушателя по клику на родительский элемент
+galleryEl.addEventListener('click', onClick);
+
+// ! Колбэк функция на слушателя
+function onClick (event) {
+    // ? Запретил действия браузера по умолчанию
+    event.preventDefault();
+
+    // ? Скрипт библиотеки
+    let lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
+
+    lightbox.on();
+  };
